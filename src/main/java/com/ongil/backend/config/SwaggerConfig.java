5df51f33c4cd50571e101489e13f2c132b55c799
kangcheolung <skhu202114111@gmail.com>
2025-12-12 17:49:39 +0900

    feat: Swagger 설정 및 스프링부트 버전 안정화
    - Spring Boot 버전 3.3.5로 안정화
    - Swagger UI 추가

---
+++ src/main/java/com/ongil/backend/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.ongil.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Ongil API Documentation")
+				.description("Ongil 백엔드 API 문서입니다.")
+				.version("v1.0.0"));
+
+	}
+}
+
