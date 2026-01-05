19968701034e1fa4d0c963f0130166f1c2b21669
marshmallowing <yuin1111801@naver.com>
2026-01-06 02:30:43 +0900

    chore: BaseEntity 추가

--- src/main/java/com/ongil/backend/domain/wishlist/entity/Wishlist.java
+++ src/main/java/com/ongil/backend/domain/wishlist/entity/Wishlist.java
@@ -1,11 +1,8 @@
 package com.ongil.backend.domain.wishlist.entity;
 
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
-
 import com.ongil.backend.domain.product.entity.Product;
 import com.ongil.backend.domain.user.entity.User;
+import com.ongil.backend.global.common.entity.BaseEntity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -17,15 +14,12 @@ import lombok.NoArgsConstructor;
 @Table(name = "wishlists")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Wishlist {
+public class Wishlist extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "wishlist_id")
 	private Long id;
-
-	@CreationTimestamp
-	@Column(name = "created_at", nullable = false, updatable = false)
-	private LocalDateTime createdAt;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
