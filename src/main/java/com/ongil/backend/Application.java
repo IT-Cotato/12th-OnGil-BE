0f39c335a7f67817869c7be012b8d0afbafdfe7e
kangcheolung <skhu202114111@gmail.com>
2026-01-15 16:33:05 +0900

    chore: 스웨거 오류 테스트

--- src/main/java/com/ongil/backend/Application.java
+++ src/main/java/com/ongil/backend/Application.java
@@ -10,8 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableFeignClients
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
