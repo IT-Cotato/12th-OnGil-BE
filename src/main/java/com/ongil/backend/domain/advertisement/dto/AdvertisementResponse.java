987177b00ee2693e143486ca29be01609d013e84
JO HYUNGJOON <chj78951@naver.com>
2026-02-12 11:02:40 +0900

    fix: 광고 API 코드 스타일 프로젝트 컨벤션에 맞게 통일

--- src/main/java/com/ongil/backend/domain/advertisement/dto/AdvertisementResponse.java
+++ src/main/java/com/ongil/backend/domain/advertisement/dto/AdvertisementResponse.java
@@ -1,12 +1,22 @@
 package com.ongil.backend.domain.advertisement.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "할인 광고 응답")
 public record AdvertisementResponse(
-        Long id,
-        String imageUrl,
-        String title,
-        String description
+
+	@Schema(description = "광고 ID")
+	Long id,
+
+	@Schema(description = "광고 이미지 URL")
+	String imageUrl,
+
+	@Schema(description = "광고 제목")
+	String title,
+
+	@Schema(description = "광고 문구")
+	String description
 ) {
 }
